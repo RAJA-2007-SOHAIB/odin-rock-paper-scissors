e33062e55de20e78f4d6eaa4a761527edf386477
sohaib <rajashahzaibabdullah8467@gmail.com>
2025-08-07 15:05:46 +0500

    Add functions to get human input and play a round
    defined a function to input players choice and a function to play a
    single round.

--- script.js
+++ script.js
@@ -4,3 +4,42 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+function getHumanChoice() {
+    const choice = prompt("Select your choice(rock, paper, scissors): ");
+    return choice;
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function showChoices(humanChoice, computerChoice) {
+    return `You chose: ${humanChoice.toLowerCase()}\nComputer chose: ${computerChoice}\n`;
+}
+
+
+function playGround(human, computer) {
+    human = human.toLowerCase();
+    let result;
+    if (human === computer) {
+        result = showChoices(human, computer) + "It's a Tie!";
+        return result;
+    } else if (human !== computer) {
+        result = (human === "rock" && computer === "scissors") ? showChoices(human, computer) + "You won!":
+    (human === "rock" && computer === "paper") ? showChoices(human, computer) + "You lost!":
+    (human === "paper" && computer === "rock") ? showChoices(human, computer) + "You won!":
+    (human === "paper" && computer === "scissors") ? showChoices(human, computer) + "You lost":
+    (human === "scissors" && computer === "paper") ? showChoices(human, computer) + "You won!":
+    (human === "scissors" && computer === "rock") ? showChoices(human, computer) + "You lost!":
+    showChoices(human, computer) + "Are You Dumb!!!";
+    }
+    return result;
+}
+
+
+
+function playGame() {
+    console.log(playGround(getHumanChoice(), getComputerChoice()));
+}
+
+playGame()
